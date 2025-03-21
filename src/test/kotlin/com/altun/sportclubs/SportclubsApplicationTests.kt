9c9bd4f9b37a1c0e1441d5a469dfc07326623ca7
Ali Deniz Altun <ada@prismacsi.com>
2025-03-21 14:07:15 +0100

    add gitignore file

--- src/test/kotlin/com/altun/sportclubs/SportclubsApplicationTests.kt
+++ src/test/kotlin/com/altun/sportclubs/SportclubsApplicationTests.kt
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest
 @SpringBootTest
 class SportclubsApplicationTests {
 
-	@Test
-	fun contextLoads() {
-	}
+    @Test
+    fun contextLoads() {
+    }
 
 }
