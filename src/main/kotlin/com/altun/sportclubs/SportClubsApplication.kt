d1c125953d45325e4e39bff6c4f101dec024139c
Ali Deniz Altun <ada@prismacsi.com>
2025-03-21 14:07:15 +0100

    add gitignore file

--- src/main/kotlin/com/altun/sportclubs/SportClubsApplication.kt
+++ src/main/kotlin/com/altun/sportclubs/SportClubsApplication.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class SportclubsApplication
+class SportClubsApplication
 
 fun main(args: Array<String>) {
-	runApplication<SportclubsApplication>(*args)
+    runApplication<SportClubsApplication>(*args)
 }
